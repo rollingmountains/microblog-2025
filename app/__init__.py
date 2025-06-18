271b46232ee3a2cf0ce0637a68f68d1369f9557f
rollingmountains <balakrishnapaudel@gmail.com>
2025-06-18 17:41:09 +0545

    add jinja templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-print(app)
-
-print(__name__)
 
 
 def registration():
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return f'Hello Flask app'
+    user = {'username': 'Sunshine'}
+    posts = [
+        {
+            'author': {'username': 'john'},
+            'post': 'This is a beautiful day'
+        },
+        {
+            'author': {'username': 'susan'},
+            'post': 'Life is a beautiful ride'
+        },
+        {
+            'author': {'username': 'will'},
+            'post': 'Live life the fullest'
+        },
+        {
+            'author': {'username': 'linda'},
+            'post': 'Life is a one jolly bus ride'
+        }
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
