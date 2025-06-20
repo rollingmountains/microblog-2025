a6785405e0980af9a465e707cf8699ba7d005398
rollingmountains <balakrishnapaudel@gmail.com>
2025-06-20 09:14:25 +0545

    add user model and setup migration

---
+++ app/models.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+from typing import Optional
+
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
+    email: so.Mapped[str] = so.mapped_column(
+        sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.username}, {self.email})'
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-my-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'SQLALCHEMY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
