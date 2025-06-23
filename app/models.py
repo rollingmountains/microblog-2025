0a2bc1330040733b7e798e65e6fdbbdc1b1292fd
rollingmountains <balakrishnapaudel@gmail.com>
2025-06-23 09:28:42 +0545

    add models and db migration using alembic

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from typing import Optional
+from datetime import datetime, timezone
 
 
 class User(db.Model):
@@ -11,5 +12,22 @@ class User(db.Model):
         sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+
     def __repr__(self):
         return f'{self.__class__.__name__}({self.username}, {self.email})'
+
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc), index=True)
+    user_id: so.Mapped[int] = so.mapped_column(
+        sa.ForeignKey(User.id), index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.user_id}, {self.body})'
